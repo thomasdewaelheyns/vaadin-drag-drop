49a34ba92b9395d9c251e6ad7193ef7799a8c0d4
Thomas De Waelheyns <thomas.dewaelheyns@qaelum.com>
2024-03-26 10:30:50 +0100

    Initial commit

--- src/main/java/com/example/application/views/MainLayout.java
+++ src/main/java/com/example/application/views/MainLayout.java
@@ -1,6 +1,6 @@
 package com.example.application.views;
 
-import com.example.application.views.draganddrop.DraganddropView;
+import com.example.application.views.draganddrop.DragAndDropView;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.html.Footer;
@@ -50,7 +50,7 @@ public class MainLayout extends AppLayout {
     private SideNav createNavigation() {
         SideNav nav = new SideNav();
 
-        nav.addItem(new SideNavItem("Drag and drop", DraganddropView.class, LineAwesomeIcon.GLOBE_SOLID.create()));
+        nav.addItem(new SideNavItem("Drag and drop", DragAndDropView.class, LineAwesomeIcon.GLOBE_SOLID.create()));
 
         return nav;
     }
